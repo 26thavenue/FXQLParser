124c2ec6ecaa24ac306ce2dc2bfb6a5b082be9ef
unknown <oni.oluwatomiwa@stu.cu.edu.ng>
2024-11-22 12:20:26 +0100

    Finished Docker file

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/26thavenue/FXQLParser
 
-go 1.21.1
+go 1.23
+
+toolchain go1.23.3
---
+++ middlewares/rateLimiter.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"math"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+type RateLimiter struct {
+	Period  time.Duration
+	MaxRate int64
+	DB   any
+}
+
+func (r RateLimiter) writeHeaders (
+	w http.ResponseWriter,
+	used int64,
+	expireTime time.Duration,
+){
+	limit := r.MaxRate
+	remaining := int64(math.Max(float64(limit-used), 0))
+	reset := int64(math.Ceil(expireTime.Seconds()))
+
+	w.Header().Add("X-RateLimit-Limit", strconv.FormatInt(limit, 10))
+	w.Header().Add("X-RateLimit-Remaining", strconv.FormatInt(remaining, 10))
+	w.Header().Add("X-RateLimit-Reset", strconv.FormatInt(reset, 10))
+
+}
